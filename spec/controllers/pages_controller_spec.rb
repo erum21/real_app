990036def72c7ca0b5550daa5aa3a8cc8ffb05f4
Edwin Vega <edwin.ponc@gmail.com>
2010-06-09 23:08:29 -0400

    Finishing with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
   describe PagesController do
+  	integrate_views
 
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+  	  get 'home'
+  	  response.should have_tag("title",
+  	                           "About Posting Comments and Feedbacks! App | Home")
     end
   end
 
@@ -15,5 +22,26 @@ require 'spec_helper'
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+  	  get 'contact'
+  	  response.should have_tag("title",
+  	                           "About Posting Comments and Feedbacks! App | Contact")
+    end
+  end
+  
+  describe "GET 'about'" do
+  	it "should be successful" do
+  	  get 'about'
+  	  response.should be_success
+  	end
+  
+    it "should have the right title" do
+  	   get 'about'
+  	   response.should have_tag("title",
+  	                            "About Posting Comments and Feedbacks! App | About")
+  	end
   end
 end
+  	                         
+  	                      
