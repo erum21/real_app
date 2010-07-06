06be1e660a6e1800fdb5d32bb910e07a3532ace2
Edwin Vega <edwin.ponc@gmail.com>
2010-07-05 22:56:18 -0400

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -44,5 +44,29 @@ describe UsersController do
       response.should have_tag("h2>img", :class => "gravatar")
     end
   end
+  
+  describe "POST 'create'" do
+
+    describe "success" do
+
+      before(:each) do
+        @attr = { :name => "New User", :email => "user@example.com",
+                  :password => "foobar", :password_confirmation => "foobar" }
+        @user = Factory(:user, @attr)
+        User.stub!(:new).and_return(@user)
+        @user.should_receive(:save).and_return(true)
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(@user))
+    end
+    
+    it "should have a welcome message" do
+    	post :create, :user => @attr
+    	flash[:success].should =~ /welcome to the realcomment app/i
+	end
+  end
+ end
 end
 
